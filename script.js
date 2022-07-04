9f8fb58478fc2598a1161211b5d94f12cbd53733
Bethrand <bethrand2019@gmail.com>
2022-07-04 15:43:59 +0100

    changes

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('Hello World!');
+
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navBar = document.querySelector('.nav-bar')
+
+
+
+hamburgerMenu.addEventListener('click', () => {
+   navBar.classList.toggle('block');
+//    navBar.classList.toggle('none')
+})
